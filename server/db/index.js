442b4b49000f04c58365c645cd94edf78f6ca5ae
Paweł Wierciński <47865579+pawelwier@users.noreply.github.com>
2020-09-22 19:27:17 +0200

    crud done

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
-const pool = new Pool({
-    user: process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-});
+const pool = new Pool(
+//     {
+//     user: process.env.PGUSER,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT,
+// }
+);
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
--- server/server.js
+++ server/server.js
@@ -10,34 +10,62 @@ app.use(express.json());
 
 app.get('/api/v1/restaurants', async (req, res) => {
 
-    const result = await db.query('select * from restaurants')
-    console.log(result);
+    const results = await db.query('select * from restaurants')
+    console.log(results);
     res.status(200).json({
         status: "success",
+        results: results.rows.length,
         data: {
-            restaurant: [
-                "bao bao",
-                "bambus"
-            ]
+            restaurant: results.rows
         }
     })
 });
 
-app.get('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params);
+app.get('/api/v1/restaurants/:id', async (req, res) => {
+    const results = await db.query(`select * from restaurants where id = $1`, [req.params.id])
+    res.status(200).json({
+        status: "success",
+        results: results.rows[0]
+    })
 })
 
-app.post('/api/v1/restaurants', (req, res) => {
-    console.log(req.body);
+app.post('/api/v1/restaurants', async (req, res) => {
+    const results = await db.query('insert into restaurants (name, location, price_range) values ($1, $2, $3) returning *', [
+        req.body.name,
+        req.body.location,
+        req.body.price_range
+    ]);
+    console.log(results);
+    res.status(200).json({
+        status: "success",
+        results: results.rows[0]
+    })
+    
 })
 
-app.put('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params.id);
+app.put('/api/v1/restaurants/:id', async (req, res) => {
+    const results = await db.query('update restaurants set name = $1, location = $2, price_range = $3 where id = $4 returning *', [
+        req.body.name,
+        req.body.location,
+        req.body.price_range,
+        req.params.id
+    ]);
+    console.log(results);
+    res.status(200).json({
+        status: "success",
+        results: results.rows[0]
+    })
+
 })
 
-app.delete('/api/v1/restaurants/:id', (req, res) => {
-    console.log(req.params.id);
-    res.status(204);
+app.delete('/api/v1/restaurants/:id', async (req, res) => {
+    const results = await db.query('delete from restaurants where id = $1', [
+        req.params.id
+    ]);
+    console.log(results);
+    res.status(204).json({
+        status: "success",
+    })
 })
 
 const port = process.env.PORT || 3001;
